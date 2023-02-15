63e2d43a2b4cdd94754a0c40c58762ce4f5f4469
hasanisu <hasanjimam@gmail.com>
2023-02-15 18:20:42 +0900

    nav bar update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {RouterProvider} from "react-router-dom";
+import router from './utilities/routes';
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>hi I am Sho@p</h2>
+        </div>
+    );
+};
+
+export default Shop;
